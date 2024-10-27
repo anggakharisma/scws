bf527399c7b06806c9c17af8be4834daf49a09df
Angga Kharisma <anggakharisma@protonmail.com>
2024-10-27 21:55:08 +0700

    chore: some adjustment

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -5,7 +5,7 @@ export default function Navbar() {
     <header className="w-full py-4 flex shadow-md">
       <div className="w-full md:w-10/12 mx-auto flex items-center justify-between gap-8 py-0 px-8 lg:px-16">
         <Link href="/"><h1 className="text-3xl font-bold text-primary">SCWS</h1></Link>
-        <ul className="w-1/2 self-center items-center justify-self-center md:flex justify-between p-4 px-8 hidden">
+        <ul className="w-1/2 self-center items-center justify-self-center lg:flex justify-between p-4 px-8 hidden">
           <Link href="/">Home</Link>
           <Link href="/">Contact</Link>
           <Link href="/">About Us</Link>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,15 @@ import Ignite from '@/app/assets/images/ignites.jpg';
 import Image from 'next/image';
 export default function Home() {
   return (
-    <div className="max-w-max w-full md:w-10/12 px-4 p-2 lg:px-8 mx-auto">
-      <div className="bg-primary lg:pl-10 pb-10 md:pb-0 flex flex-col-reverse md:flex-row justify-between rounded-2xl relative mb-40">
+    <div className="max-w-max w-full lg:w-10/12 px-4 p-2 lg:px-8 mx-auto">
+      <div className="bg-primary lg:pl-10 pb-10 lg:pb-0 flex flex-col-reverse lg:flex-row justify-between rounded-2xl relative mb-40">
         <div className="w-full h-full rounded-3xl absolute bg-secondary left-0 -top-[10px] -z-10"></div>
-        <div className="flex flex-col w-5/6 mt-8 md:w-1/3 justify-center mx-auto">
-          <h1 className="text-[#171717] text-3xl md:text-5xl md:leading-[4rem] font-bold w-5/6">Building Strong Mind Strong Community</h1>
+        <div className="flex flex-col w-5/6 mt-8 lg:w-1/3 justify-center mx-auto">
+          <h1 className="text-[#171717] text-3xl lg:text-5xl lg:leading-[4rem] font-bold w-5/6">Building Strong Mind Strong Community</h1>
           <button className='bg-[#061D3D] text-white rounded-xl p-6 py-2 text-left self-start my-4'>Enroll Now</button>
         </div>
         <Image className='rounded-[20px]' src={Hero} alt='hero images with students talking as discussing lesson' />
-        <div className='absolute shadow-md -bottom-[4rem] md:-bottom-10 md:left-20 flex left-[50%] -translate-x-1/2 md:translate-x-0'>
+        <div className='absolute shadow-lg -bottom-[4rem] lg:-bottom-14 lg:left-20 flex left-[50%] -translate-x-1/2 lg:translate-x-0'>
           {
             [
               {
@@ -27,7 +27,7 @@ export default function Home() {
                 title: "Courses"
               }
             ].map((item, id) => {
-              return <div key={id} className='md:flex flex-col items-center bg-white p-4 md:p-6'>
+              return <div key={id} className='lg:flex flex-col items-center bg-white p-4 lg:p-6'>
                 <h3 className='font-bold'>{item.count}</h3>
                 <p>{item.title}</p>
               </div>
@@ -37,7 +37,7 @@ export default function Home() {
       </div>
 
 
-      <div className='mt-24 flex flex-col md:flex-row gap-8 w-10/12 mx-auto mb-80'>
+      <div className='mt-24 flex flex-col lg:flex-row gap-8 w-10/12 mx-auto mb-12'>
         <div className='bg-red-400 w-full h-[480px] relative'>
           <div className='absolute w-full h-full -top-4 -left-4 bg-primary'></div>
           <div className='absolute w-full h-full -top-8 -left-8 bg-secondary -z-10'></div>
@@ -48,13 +48,27 @@ export default function Home() {
             alt="ignites" />
         </div>
         <div className='p-0'>
-          <h1 className='text-4xl md:text-5xl w-full md:w-3/6 font-bold mb-8'>Where Education Ignites</h1>
+          <h1 className='text-4xl lg:text-5xl w-full lg:w-3/6 font-bold mb-8'>Where Education Ignites</h1>
           <p className='mb-8 text-lg leading-loose'>{"At SCWS, education isn't just about memorizing facts and figures. It's about igniting a passion for learning that will stay with you long after you graduate. Our classrooms are vibrant hubs of curiosity and exploration, where teachers act as guides, mentors, and fellow explorers on the journey of knowledge."}<br /><br />
 
             {"Here at SCWS, you'll be challenged to think critically, ask insightful questions, and delve deeper into subjects that spark your interest. We foster a collaborative environment where students learn from each other, share ideas, and build the confidence to express themselves."}
           </p>
-
         </div>
+      </div>
+
+
+      <div className='mt-0 gap-8 w-10/12 mx-auto mb-80 grid grid-cols-4 bg-yellow-300 justify-between items-center place-items-center
+      '>
+        <div className='justify-self-end self-end'>
+          <p>lorem</p>
+        </div>
+        <p>A</p>
+        <p>A</p>
+        <p>A</p>
+        <p>A</p>
+        <p>A</p>
+        <p>A</p>
+        <p>A</p>
       </div>
     </div>
   )
