ca29117cbc27937ba605da3d610ce28f247799d2
Angga Kharisma <anggakharisma@protonmail.com>
2024-11-02 03:02:23 +0700

    chore: finishing info section

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,11 +1,34 @@
 "use client"
-import Hero from '@/app/assets/images/hero.jpg';
 import ArtsCulture from '@/app/assets/images/arts_and_culture.jpg';
-import Teacher from '@/app/assets/images/teachers.jpg';
 import Awards from '@/app/assets/images/awards.jpg';
-import Ignite from '@/app/assets/images/ignites.jpg';
 import Career from '@/app/assets/images/career.jpg';
+import Hero from '@/app/assets/images/hero.jpg';
+import Ignite from '@/app/assets/images/ignites.jpg';
+import Teacher from '@/app/assets/images/teachers.jpg';
 import Image from 'next/image';
+
+const InfoCardImage = ({
+  children,
+  image,
+  classCustom
+}: {
+  children: React.ReactNode,
+  image: string,
+  classCustom: string
+}) => {
+  return (
+    <div
+      className={`${classCustom} bg-cover bg-center bg-no-repeat hover:border-2 hover:border-primary`}
+      style={{
+        backgroundImage: `url(${image})`,
+        width: "100%",
+        height: "100%"
+      }}
+    >
+      {children}
+    </div>
+  )
+}
 
 export default function Home() {
   return (
@@ -14,7 +37,7 @@ export default function Home() {
         <div className="w-full h-full rounded-3xl absolute bg-secondary left-0 -top-[10px] -z-10"></div>
         <div className="flex flex-col w-5/6 mt-8 lg:w-1/3 justify-center mx-auto">
           <h1 className="text-[#171717] md:text-3xl text-xl lg:text-5xl lg:leading-[4rem] font-bold w-5/6">Building Strong Mind Strong Community</h1>
-          <button className='bg-[#061D3D] text-white rounded-xl p-6 py-2 text-left self-start my-4'>Enroll Now</button>
+          <button className='bg-[#061D3D] text-white rounded-xl p-6 py-2 text-left self-start my-4 hover:filter hover:invert transition-all'>Enroll Now</button>
         </div>
         <Image className='rounded-[20px]' src={Hero} alt='hero images with students talking as discussing lesson' />
         <div className='absolute shadow-lg -bottom-[4rem] lg:-bottom-14 lg:left-24 flex left-[50%] -translate-x-1/2 lg:translate-x-0'>
@@ -66,68 +89,45 @@ export default function Home() {
         </div>
       </div>
 
-      <div className='md-0 md:mt-24 gap-0 w-10/12 mx-auto min-w-40 mb-80 grid md:grid-cols-2 grid-cols-1 lg:grid-cols-4 justify-between items-center'>
-        <div className='p-8 bg-secondary min-h-56 flex flex-col justify-between'>
+      {
+        // Info Section
+      }
+      <div className='md-0 md:mt-24 gap-0 w-10/12 mx-auto min-w-40 mb-20 grid md:grid-cols-2 grid-cols-1 lg:grid-cols-4 justify-between items-center '>
+        <div className='p-8 bg-secondary min-h-56 flex flex-col justify-between transition-all hover:cursor-pointer'>
           <h3 className='text-primary font-bold md:text-3xl text-xl'>Activity</h3>
           <p className='text-white self-end items-end'>Activity</p>
         </div>
-        <div
-          className='p-8 min-h-56 flex items-center col-span-0 lg:col-span-2 relative'
-          style={{
-            width: "100%",
-            height: "100%",
-            backgroundPosition: "100%",
-            backgroundSize: "100%",
-            backgroundImage: `url(${ArtsCulture.src})`
-          }}
-        >
+
+        <InfoCardImage image={ArtsCulture.src} classCustom='p-8 min-h-56 w-full h-full flex items-center col-span-0 lg:col-span-2 relative hover:cursor-pointer bg-cover bg-center'>
           <div className='bg-black opacity-40 absolute top-0 left-0 bottom-0 right-0 w-full h-full'></div>
           <h3 className='text-white md:text-3xl text-xl font-bold z-10'>{'ARTS & CULTURE'}</h3>
-        </div>
-        <div className='p-8 bg-gray-400 min-h-56 flex justify-center items-center relative'
-          style={{
-            width: "100%",
-            height: "100%",
-            backgroundPosition: "100%",
-            backgroundSize: "100%",
-            backgroundImage: `url(${Teacher.src})`
-          }}
-        >
+        </InfoCardImage>
+
+        <InfoCardImage image={Teacher.src} classCustom='p-8 bg-gray-400 min-h-56 flex justify-center items-center relative hover:cursor-pointer'>
           <div className='bg-black opacity-40 absolute top-0 left-0 bottom-0 right-0 w-full h-full'></div>
           <h3 className='text-white z-10 font-bold md:text-3xl text-xl'>TEACHERS</h3>
-        </div>
-        <div className='p-8 bg-gray-400 min-h-56 flex justify-center items-center relative'
-          style={{
-            width: "100%",
-            height: "100%",
-            backgroundPosition: "100%",
-            backgroundSize: "100%",
-            backgroundImage: `url(${Awards.src})`
-          }}
-        >
+        </InfoCardImage>
+
+        <InfoCardImage image={Awards.src} classCustom='p-8 bg-gray-400 min-h-56 flex justify-center items-center relative hover:cursor-pointer'>
           <div className='bg-black opacity-40 absolute top-0 left-0 bottom-0 right-0 w-full h-full'></div>
           <h3 className='text-white font-bold md:text-3xl text-xl z-10'>AWARDS</h3>
-        </div>
-        <div className='p-8 bg-white min-h-56 flex justify-center items-center'>
+        </InfoCardImage>
+        <div className='p-8 bg-white min-h-56 flex justify-center items-center hover:cursor-pointer'>
           <h3 className='text-black font-bold md:text-3xl text-xl'>GRADUATES</h3>
         </div>
-        <div
-          className='p-8 bg-gray-400 min-h-56 flex justify-center items-center relative'
-          style={{
-            backgroundSize: "cover",
-            backgroundPosition: '100%',
-            backgroundRepeat: 'no-repeat',
-            backgroundImage: `url(${Career.src})`
-          }}
-        >
-          <div className='bg-black opacity-40 absolute top-0 left-0 bottom-0 right-0 w-full h-full'></div>
-          <h3 className='text-white font-bold md:text-3xl text-xl z-10'>CARRERS</h3>
-        </div>
 
-        <div className='p-8 bg-secondary min-h-56 flex justify-center items-center'>
+
+        <InfoCardImage image={Career.src} classCustom='p-8 bg-gray-400 min-h-56 flex justify-center items-center relative hover:cursor-pointer'>
+          <div className='bg-black opacity-40 absolute top-0 left-0 bottom-0 right-0 w-full h-full hover:cursor-pointer'></div>
+          <h3 className='text-white font-bold md:text-3xl text-xl z-10'>CARRERS</h3>
+        </InfoCardImage>
+
+        <div className='p-8 bg-secondary min-h-56 flex justify-center items-center hover:cursor-pointer'>
           <h3 className='text-secondary font-bold md:text-3xl text-xl'>GRADUATES</h3>
         </div>
       </div>
-    </div>
+
+      <h1>FOOTER</h1>
+    </div >
   )
 }
