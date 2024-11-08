ae7e8496079c634e9fa6c97425ab6c13477b29ad
Angga Kharisma <anggakharisma@protonmail.com>
2024-11-08 21:00:01 +0700

    chore: slow

--- src/app/(main)/login/LoginForm.tsx
+++ src/app/(main)/login/LoginForm.tsx
@@ -28,62 +28,66 @@ export default function LoginForm() {
     console.log(data);
   };
   return (
-    <Box
-      component="form"
-      onSubmit={handleSubmit(onSubmit)}
-      position='relative'
-      sx={{
-        width: '100%',
-        maxWidth: 400,
-        px: 4,
-        py: 8,
-        border: '1px solid #ddd',
-        borderRadius: 2,
-        boxShadow: "2px 8px 10px rgba(0,0,0,0.09)"
-      }}
-    >
-      <p onClick={() => router.back()} className='absolute -right-4 -top-4 bg-red-500 text-white font-bold flex justify-center items-center rounded-full w-10 h-10 text-center hover:cursor-pointer'>
-        X
-      </p>
-      <Box
-        display="flex"
-        flexDirection='column'
-        gap={1}
-      >
-        <h1 className="text-3xl font-bold text-primary">SCWS</h1>
-        <Typography variant="h6" gutterBottom>
-          Enter your credentials
-        </Typography>
-      </Box>
-      <TextField
-        autoFocus
-        {...register('email')}
-        label="Email"
-        variant="outlined"
-        margin="normal"
-        error={!!errors.email}
-        helperText={errors.email?.message}
-        fullWidth
-      />
+    <>
+      {
+        isClient &&
+        <Box
+          component="form"
+          onSubmit={handleSubmit(onSubmit)}
+          position='relative'
+          sx={{
+            width: '100%',
+            maxWidth: 400,
+            px: 4,
+            py: 8,
+            border: '1px solid #ddd',
+            borderRadius: 2,
+            boxShadow: "2px 8px 10px rgba(0,0,0,0.09)"
+          }}
+        >
+          <p onClick={() => router.back()} className='absolute -right-4 -top-4 bg-red-500 text-white font-bold flex justify-center items-center rounded-full w-10 h-10 text-center hover:cursor-pointer'>
+            X
+          </p>
+          <Box
+            display="flex"
+            flexDirection='column'
+            gap={1}
+          >
+            <h1 className="text-3xl font-bold text-primary">SCWS</h1>
+            <Typography variant="h6" gutterBottom>
+              Enter your credentials
+            </Typography>
+          </Box>
+          <TextField
+            autoFocus
+            {...register('email')}
+            label="Email"
+            variant="outlined"
+            margin="normal"
+            error={!!errors.email}
+            helperText={errors.email?.message}
+            fullWidth
+          />
 
-      <TextField
-        {...register('password')}
-        label="Password"
-        type="password"
-        variant="outlined"
-        margin="normal"
-        error={!!errors.password}
-        helperText={errors.password?.message}
-        fullWidth
-        sx={{
-          mb: 4
-        }}
-      />
+          <TextField
+            {...register('password')}
+            label="Password"
+            type="password"
+            variant="outlined"
+            margin="normal"
+            error={!!errors.password}
+            helperText={errors.password?.message}
+            fullWidth
+            sx={{
+              mb: 4
+            }}
+          />
 
-      <Button type="submit" variant="contained" color="primary" fullWidth>
-        Login
-      </Button>
-    </Box>
+          <Button type="submit" variant="contained" color="primary" fullWidth>
+            Login
+          </Button>
+        </Box>
+      }
+    </>
   )
-
 }
